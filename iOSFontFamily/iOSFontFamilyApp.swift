c4f0c80b930772f36f50c8a39fa09b08424a9b73
GroTion <grotion0720@gmail.com>
2021-04-04 21:02:44 +0800

    Initial Commit

---
+++ iOSFontFamily/iOSFontFamilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFontFamilyApp.swift
+//  iOSFontFamily
+//
+//  Created by Shaun Ku on 2021/4/4.
+//
+
+import SwiftUI
+
+@main
+struct iOSFontFamilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
